8f25d86f6dcd3915c2fff586a2d934fd5aa9fe13
umbrafin <fin211314@aliyun.com>
2019-07-04 23:57:58 +0800

    Initial commit

---
+++ src/main/java/com/umbra/javaBasic/Main.java
@@ -0,0 +1,20 @@
+package com.umbra.javaBasic;
+
+/**
+ * java 基础练习
+ *
+ *
+ */
+public class Main {
+
+    static String quanjubianliang = "全局变量";
+
+
+    public static void main(String[] args) {
+
+        //全局变量输出
+        System.out.println(quanjubianliang);
+
+
+    }
+}
