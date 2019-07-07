4f056ce00c09131acce456432c29a9504b007178
umbrafin <fin211314@aliyun.com>
2019-07-08 00:13:57 +0800

    by Fin.

--- src/main/java/com/umbra/javaBasic/BufferedReaderDemo.java
+++ src/main/java/com/umbra/javaBasic/BufferedReaderDemo.java
@@ -1,50 +1,38 @@
 package com.umbra.javaBasic;
 
-
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 
-/**
- * java 基础练习
- */
-public class FileReaderDemo {
-
-
+public class BufferedReaderDemo {
     public static void main(String[] args) {
 
         String filepath = "F:/workspace/logout/debug2.log";
 
-        //创建输入流对象
-        //File file = new File(filepath);
-
-        Reader reader = null;
+        BufferedReader reader = null;
 
         try {
 
-            //读取数据
-            reader = new FileReader(filepath);
-
-            int c = -1;
-            while ((c = reader.read()) != -1) {
-                System.out.print((char) c);
+            reader = new BufferedReader(new FileReader(filepath));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if(reader != null){
 
-
-        } finally {
-            //关闭输入流
-            if (reader != null) {
                 try {
                     reader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
             }
+
         }
     }
 }
