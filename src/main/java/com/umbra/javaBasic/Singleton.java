e10f6449f86e4d9fa6d154bbd193359ff07dc72b
umbrafin <fin211314@aliyun.com>
2019-07-07 17:00:10 +0800

    by Fin.

---
+++ src/main/java/com/umbra/javaBasic/Singleton.java
@@ -0,0 +1,34 @@
+package com.umbra.javaBasic;
+
+/**
+ * 单例模式
+ * 只能 创建一个对象时，用到类似的写法；
+ */
+public class Singleton {
+
+    private static Singleton s;
+
+    private String output = "testSingleton";
+
+
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (s == null) {
+            s = new Singleton();
+        }
+        return s;
+    }
+
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+}
--- src/test/java/JavaBasicTest.java
+++ src/test/java/JavaBasicTest.java
@@ -1,9 +1,19 @@
+import com.umbra.javaBasic.Singleton;
 import org.junit.Test;
 
 public class JavaBasicTest {
 
     @Test
     public void testStatic() throws Exception{
-        System.out.println("123");
+        Singleton s0 = Singleton.getInstance();
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.format("s0 内存地址：%s\n", s0);
+        System.out.format("s1 内存地址：%s\n", s1);
+        System.out.format("s2 内存地址：%s\n", s2);
+        System.out.format("s0.output：%s\n", s0.getOutput());
+
+
     }
 }
